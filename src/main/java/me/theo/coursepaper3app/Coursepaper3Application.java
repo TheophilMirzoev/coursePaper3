444181fbaf60a533aefd273e2f3cb2a7bb1c6376
TheophilMirzoev <115739938+TheophilMirzoev@users.noreply.github.com>
2023-04-10 08:51:55 +0300

    f

---
+++ src/main/java/me/theo/coursepaper3app/Coursepaper3Application.java
@@ -0,0 +1,13 @@
+package me.theo.coursepaper3app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Coursepaper3Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Coursepaper3Application.class, args);
+    }
+
+}
---
+++ src/test/java/me/theo/coursepaper3app/Coursepaper3ApplicationTests.java
@@ -0,0 +1,13 @@
+package me.theo.coursepaper3app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Coursepaper3ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
